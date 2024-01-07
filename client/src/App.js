ced65e945a1ce228903a18d58e469e8b2bf4da2b
avinasdube <avidubey712@gamil.com>
2024-01-08 01:43:43 +0530

    [modified] : modified and added layout component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
-import './styles/index.css';
-import { sayHi } from './api/api';
+import React from 'react';
+import './styles/index.scss';
+import MainLayout from './layouts/MainLayout/MainLayout';
 
 function App() {
-  const [result, setResult] = useState(null);
-
-  const hello = async () => {
-    try {
-      const response = await sayHi();
-      setResult(response.data);
-    } catch (error) {
-      console.log("Error calling API", error)
-    }
-  }
-
+  
   return (
     <div className="App">
-      <button onClick={hello}>westream</button>
-      <label>{result}</label>
+      <MainLayout />
     </div>
   )
 }
