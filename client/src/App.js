254d655966a96182781b35b05e5294110ba3670d
avinasdube <avidubey712@gamil.com>
2024-01-07 12:01:09 +0530

    [modified] : created button to test server response

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
+import './styles/index.css';
+import { sayHi } from './api/api';
 
 function App() {
+  const [result, setResult] = useState(null);
+
+  const hello = async () => {
+    try {
+      const response = await sayHi();
+      setResult(response.data);
+    } catch (error) {
+      console.log("Error calling API", error)
+    }
+  }
+
   return (
     <div className="App">
-      westream
+      <button onClick={hello}>westream</button>
+      <label>{result}</label>
     </div>
-  );
+  )
 }
 
 export default App;
