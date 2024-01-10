d3b31df364e980c7db9f028d1069afca530be481
avinasdube <avidubey712@gamil.com>
2024-01-10 23:14:22 +0530

    [new] : added new delete feature

--- client/src/components/ViewBar/ViewBar.js
+++ client/src/components/ViewBar/ViewBar.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './ViewBar.scss';
+import dots from '../../assets/icons/dots.png';
 import MoreVideos from '../MoreVideos/MoreVideos';
-import { API_BASE_URL, getVideos } from '../../api/api';
+import { API_BASE_URL, deleteVideo, getVideos } from '../../api/api';
 import { API_ENDPOINTS } from '../../utils/constants';
 
 const ViewBar = () => {
+  // defining various states
+  const [delOpen, setDelOpen] = useState(false);
   const [videos, setVideos] = useState([]);
   const [currentVideo, setCurrentVideo] = useState(null);
   const [currentSubtitle, setCurrentSubtitle] = useState(null);
@@ -34,12 +37,28 @@ const ViewBar = () => {
     const newSubtitle = currentVideo?.subtitles.find(
       (subtitle) =>
         playedSeconds >= parseTimestamp(subtitle.timestamp) &&
-        playedSeconds <= parseTimestamp(subtitle.timestamp) + 3 // Display for 3 seconds
+        playedSeconds <= parseTimestamp(subtitle.timestamp) + 3 // display subs for 3 seconds
     );
 
     // updating the current subtitle if newSubtitle is not same
     if (newSubtitle !== currentSubtitle) {
       setCurrentSubtitle(newSubtitle);
+    }
+  }
+
+  // function to handle delete dropdown
+  const handleDelDropdown = () => {
+    delOpen === false ? setDelOpen(true) : setDelOpen(false);
+  }
+
+  // function to handle video deletion
+  const handleDelete = async () => {
+    const vid = currentVideo._id;
+    try {
+      await deleteVideo(vid)
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -56,7 +75,13 @@ const ViewBar = () => {
             />
             {currentSubtitle && <p className="subtt">{currentSubtitle.text}</p>}
           </div>
-          <div className="videoTitle">{currentVideo.title}</div>
+          <div className="titleMain">
+            <div className="videoTitle">{currentVideo.title}</div>
+            <img className="optionIcon" src={dots} alt='' onClick={handleDelDropdown}></img>
+            <div className={`delDrop ${delOpen === true ? 'active' : ''}`}>
+              <div className="delBtn" onClick={handleDelete}>Delete</div>
+            </div>
+          </div>
           <div className="videoDescrip">{currentVideo.description}</div>
         </>
       )}
